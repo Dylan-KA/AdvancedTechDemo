99b670a19b3b30c1e53847dc4a81a3cdb799ae13
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2023-10-03 22:21:09 +1100

    Finished Rarity and Materials
    Random rarity is assigned and corresponding material is applied.
    
    Also removed the legacy project.

--- RacingDemo/Source/RacingDemo/VehiclePCG.cpp
+++ RacingDemo/Source/RacingDemo/VehiclePCG.cpp
@@ -21,10 +21,88 @@ void UVehiclePCG::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	GenerateRandomVehicle();
+	UE_LOG(LogTemp, Warning, TEXT("Vehicle Rarity: %d"), (int32)VehicleRarity)
+	UE_LOG(LogTemp, Warning, TEXT("Vehicle Stats: %s"), *VehicleStats.ToString())
 	
 }
 
+void UVehiclePCG::GenerateRandomVehicle()
+{
+	VehicleRarity = VehicleRarityPicker();
+	TArray<bool> GoodStats;
+	switch (VehicleRarity)
+	{
+	case EVehicleRarity::Legendary:
+		GoodStats = VehicleStatPicker(5, 6);
+		break;
+	case EVehicleRarity::Master:
+		GoodStats = VehicleStatPicker(4, 6);
+		break;
+	case EVehicleRarity::Rare:
+		GoodStats = VehicleStatPicker(3, 6);
+		break;
+	default:
+		GoodStats = VehicleStatPicker(0, 6);
+		break;
+	}
+
+	VehicleStats.MassScale = GoodStats[0] ? FMath::RandRange(0.8f, 1.0f) : FMath::RandRange(1.0f, 5.0f);
+	//VehicleStats.CentreOfMass = GoodStats[1] ? FMath::RandRange(0.05f, 0.2f) : FMath::RandRange(0.2f, 1.0f);
+	VehicleStats.Fuel = GoodStats[2] ? FMath::RandRange(15.0f, 30.0f) : FMath::RandRange(5.0f, 15.0f);
+	VehicleStats.TopSpeed = GoodStats[3] ? FMath::RandRange(100, 300) : FMath::RandRange(50, 100);
+	VehicleStats.TurningSpeed = GoodStats[4] ? 1.0f : FMath::RandRange(0.5f, 1.0f);
+	
+}
+
+EVehicleRarity UVehiclePCG::VehicleRarityPicker()
+{
+	// Rules:
+	// 50% chance of Common
+	// 30% chance of Rare
+	// 15% chance of Master
+	// 5% chance of Legendary
+	const float RandPercent = FMath::RandRange(0.0f, 1.0f);
+	
+	if (RandPercent <= 0.5f)
+	{
+		return EVehicleRarity::Common;
+	}
+	if (RandPercent <= 0.8f)
+	{
+		return EVehicleRarity::Rare;
+	}
+	if (RandPercent <= 0.95f)
+	{
+		return EVehicleRarity::Master;
+	}
+	
+	return EVehicleRarity::Legendary;
+}
+
+TArray<bool> UVehiclePCG::VehicleStatPicker(int32 NumOfGood, int32 NumOfStats)
+{
+	// Fill the array with the correct number of good and bad stats.
+	TArray<bool> GoodStats;
+	for (int32 i = 0; i < NumOfStats; i++)
+	{
+		// Ternary condition: Will add true if I < NumOfGood otherwise add false.
+		GoodStats.Add(i < NumOfGood ? true : false);
+	}
+
+	// Array shuffling algorithm.
+	for (int32 i = 0; i < GoodStats.Num(); i++)
+	{
+		// Get a random index from the GoodStats array.
+		const int32 RandIndex = FMath::RandRange(0, GoodStats.Num() - 1);
+		// Then swap the item at that random index with the item in the i index.
+		const bool Temp = GoodStats[i];
+		GoodStats[i] = GoodStats[RandIndex];
+		GoodStats[RandIndex] = Temp;
+	}
+
+	return GoodStats;
+}
 
 // Called every frame
 void UVehiclePCG::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -38,4 +116,5 @@ void UVehiclePCG::GetVehicleBP()
 {
 	VehicleBP = GetWorld()->GetGameInstance<URacingGameInstance>()->GetVehicleClass();
 }
+
 
--- RacingDemo/Source/RacingDemo/VehiclePCG.h
+++ RacingDemo/Source/RacingDemo/VehiclePCG.h
@@ -16,8 +16,37 @@ enum class EVehicleRarity : uint8
 	Legendary
 };
 
+USTRUCT(BlueprintType)
+struct FVehicleStats
+{
+	GENERATED_BODY()
+public:
+	
+	float MassScale = 1.0f;
+	FVector CentreOfMass = FVector(0,0,0);
+	float Fuel = 10.0f;
+	float TopSpeed = 100;
+	float TurningSpeed = 1.0f;
+	float BreakingStrength = 1.0f;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+	/**
+	 * A debug ToString function that allows the easier printing of the weapon stats.
+	 * @return A string detailing the weapon stats stored in this struct.
+	 */
+	FString ToString() const
+	{
+		FString WeaponString = "";
+		WeaponString += "MassScale:      " + FString::SanitizeFloat(MassScale) + "\n";
+		WeaponString += "CentreOfMass:     " + CentreOfMass.ToString() + "\n";
+		WeaponString += "Fuel:   " + FString::SanitizeFloat(Fuel) + "\n";
+		WeaponString += "TopSpeed: " + FString::FromInt(TopSpeed) + "\n";
+		WeaponString += "TurningSpeed:   " + FString::SanitizeFloat(TurningSpeed) + "\n";
+		WeaponString += "BreakingStrength:   " + FString::SanitizeFloat(BreakingStrength)  + "\n";
+		return WeaponString;
+	}
+};
+
+UCLASS( BlueprintType, Blueprintable )
 class RACINGDEMO_API UVehiclePCG : public UActorComponent
 {
 	GENERATED_BODY()
@@ -37,10 +66,25 @@ protected:
 	// Generates random vehicle stats
 	void GenerateRandomVehicle();
 
-	void ApplyMaterials();
+	// Chooses a random Vehicle rarity with weighted chances
+	EVehicleRarity VehicleRarityPicker();
+
+	// Current stats of the vehicle
+	FVehicleStats VehicleStats;
+	
+	// Chooses which Stats are good and which are bad
+	TArray<bool> VehicleStatPicker(int32 NumOfGood, int32 NumOfStats);
 	
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-		
+
+	// Applies material to vehicle mesh based on rarity
+	UFUNCTION(BlueprintImplementableEvent)
+	void ApplyMaterials();
+
+	// Current rarity of the vehicle
+	UPROPERTY(BlueprintReadOnly)
+	EVehicleRarity VehicleRarity = EVehicleRarity::Common;
+	
 };
